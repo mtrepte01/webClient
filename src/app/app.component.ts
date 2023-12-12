e60e6e9427827127bb5622f919cf9584bc0b8f1e
patrickhornung <patrick-hornung@gmx.de>
2023-12-12 10:32:11 +0100

    First UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ChatMessage } from './chatmessage';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html' ,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ChatClient';
+  title = 'homes';
+
+  @Input() chatMessageInput!: ChatMessage;
+
 }
