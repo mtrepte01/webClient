00accd49dab876308dba011414365043cceecf73
patrickhornung <patrick-hornung@gmx.de>
2024-01-08 10:53:19 +0100

    added ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ChatMessage } from './chatmessage';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
 
 import { PostService } from './postService';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HttpClientModule],
+  imports: [CommonModule, RouterOutlet,HttpClientModule, FormsModule],
   templateUrl: './app.component.html' ,
   providers: [],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'homes';
   posts:any;
+  
+  chatMessage = {
+    user: "",
+    message: ""
+  }
 
   constructor(private service:PostService) {
   }
 
-  @Input() chatMessageInput!: ChatMessage;
-
   public getMessage(){
-    alert('Test');
+    console.log(this.chatMessage);
+    //let input = this.chatMessage.user + " sent: " + this.chatMessage.message;
+    //alert(input);
 
-    
+    this.chatMessage.message = "";
 
     this.service.getPosts()
     .subscribe(response => {
