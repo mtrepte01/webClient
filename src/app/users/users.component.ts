db2dc67ea3f4b6bae7a0cce8e9dd719069c7926e
mtrepte01 <148533263+mtrepte01@users.noreply.github.com>
2024-01-11 07:56:13 +0100

    Add files via upload

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -4,45 +4,94 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ChatUsers } from './users';
+import { LoginComponent } from '../login/login.component';
 
 import { PostService } from '../postService';
 import { Observable } from 'rxjs';
 import { AppComponent } from '../app.component';
+import { ApplicationService } from '../applicationService';
+import { SignalRService } from '../signalRService';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [NgFor, NgClass, AppComponent, NgIf],
+  imports: [NgFor, NgClass, AppComponent, LoginComponent, NgIf],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
+  , providers: [ApplicationService]
 })
-export class UsersComponent{
+
+export class UsersComponent {
+
   display = false;
   userList: any;
   selectedUser = "";
   selectedId = "";
   users = [
     {
-        id: 0,
-        name: "",
-        currentStatus: "",
+      id: 0,
+      name: "",
+      currentStatus: "",
     },
   ];
 
-  constructor(private service: PostService) {
+  constructor(private service: PostService, public appService: ApplicationService, private signalRService: SignalRService) {
     this.getuserList();
+
+
+  }
+
+  ngOnInit() {
+    this.signalRService.startConnection();
+
+    this.signalRService.addNewUserListener((pData: any) => {
+      this.setStatus(pData, 'online');
+    });
+
+    this.signalRService.addLogoutUserListener((pData: any) => {
+      this.setStatus(pData, 'offline');
+    });
+
+    this.signalRService.addSendMessageListener((pSender: string, pReceiver: string, pMessage: string) => {
+        if(this.appService.currentUser == pReceiver){
+          alert('Nachricht von ' + pSender + ': ' + pMessage);
+        }
+    });
+  }
+
+  private setStatus(pUserName: string, pStatus: string) {
+
+    let isNewUser: boolean = true;
+
+    this.userList.forEach((pUser: any) => {
+      if (pUser.name == pUserName) {
+        isNewUser = false;
+        pUser.currentStatus = pStatus;
+      }
+    });
+
+    if (isNewUser) {
+      this.getuserList();
+    }
   }
 
   public toggleChat(username: string) {
     this.selectedUser = username;
+
+    this.userList.forEach((pUser: any) => {
+      if (pUser.name == username) {
+        this.appService.idCurrentReceiver = pUser.id;
+      }
+    });
+
     if (!this.display)
-    this.display = !this.display;
+      this.display = !this.display;
   }
 
   public getuserList(): ChatUsers[] {
     this.service.getUsers().subscribe((response) => {
       this.userList = response;
-      this.users = Array.from(this.userList);      
+      this.users = Array.from(this.userList);
 
       return this.users;
     });
@@ -50,9 +99,18 @@ export class UsersComponent{
     return [];
   }
 
+  public logout() {
 
-    //let input = this.chatMessage.user + " sent: " + this.chatMessage.message;
-    //alert(input);
+    this.service.logoutUser(this.appService.currentUser)
+      .subscribe(response => {
+        //this.posts = response;
+        this.appService.reset();
+      });
+
+  }
+
+  //let input = this.chatMessage.user + " sent: " + this.chatMessage.message;
+  //alert(input);
 
 
 }
